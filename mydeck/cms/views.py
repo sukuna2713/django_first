9b59dbd4727c09fdb74111117e4215b496f3e567
sukuna2713 <sukuna2713@gmail.com>
2020-07-29 17:13:30 +0900

    make listpage

--- mydeck/cms/views.py
+++ mydeck/cms/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from cms.models import Card
+
 # Create your views here.
 def card_list(request):
     """カードの一覧"""
-    return HttpResponse('カードの一覧')
+    #return HttpResponse('カードの一覧')
+    cards = Card.objects.all().order_by('id')
+    return reader(request,'cms/card_list.html',{'cards': cards})
 
 
 def card_edit(request, card_id = None):
