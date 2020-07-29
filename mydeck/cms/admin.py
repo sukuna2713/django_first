71f64726cbc4e3dd8c6113a1d07bc35cf6ba8b8a
sukuna2713 <sukuna2713@gmail.com>
2020-07-29 14:14:05 +0900

    add model

--- mydeck/cms/admin.py
+++ mydeck/cms/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from cms.models import Card, Deck
 
 # Register your models here.
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',) #一覧に出したい名前
+    list_display_links = ('id', 'name',) #修正リンクでクリックできる項目
+
+admin.site.register(Card, CardAdmin)
+
+class DeckAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment',) #一覧に出したい名前
+    list_display_links = ('id', 'comment',) #修正リンクでクリックできる項目
+    raw_id_fields = ('card',) #外部キーをプルダウンにしない（データ件数が増加時のタイムアウトを予防）
+
+admin.site.register(Deck, DeckAdmin)
--- mydeck/cms/models.py
+++ mydeck/cms/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    """カード"""
+    name = models.CharField('カード名', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Deck(models.Model):
+    """デッキ"""
+    name = models.CharField('デッキ名', max_length=255)
+    card = models.ForeignKey(Card, verbose_name='カード', related_name='decks', on_delete=models.CASCADE)
+    comment = models.TextField('コメント', blank=True)
+
+    def __str__(self):
+        return self.name
